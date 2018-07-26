8b800513340e8011b7dfcaa3d85908db5f9745f1
Елизавета <@192.168.1.3>
2018-07-26 15:27:55 +0400

    почти готово

---
+++ src/ch1/task20/Data.java
@@ -0,0 +1,14 @@
+package ch1.task20;
+
+public class Data {
+
+    public static String fio = "Алексеева";
+    public static String data = "25.07.2018 20:00";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+
+        }
+    }
+
+}
